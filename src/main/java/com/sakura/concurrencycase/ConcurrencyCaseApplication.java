c611bc664abd6003d77430342c912d9a0aaf08cf
sakura <sakura@sakura.com>
2019-05-04 19:05:35 +0800

    Initial commit

---
+++ src/main/java/com/sakura/concurrencycase/ConcurrencyCaseApplication.java
@@ -0,0 +1,13 @@
+package com.sakura.concurrencycase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyCaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrencyCaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sakura/concurrencycase/ConcurrencyCaseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sakura.concurrencycase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConcurrencyCaseApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
